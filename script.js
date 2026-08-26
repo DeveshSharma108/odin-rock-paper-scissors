0fb4ce038a95e9934191820090e65a9a08ef8a29
DeveshSharma108 <137079248+DeveshSharma108@users.noreply.github.com>
2026-08-26 22:53:46 +0530

    add function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hi");
+function getComputerChoice() {
+  const choices = {
+    0: "rock",
+    1: "paper",
+    2: "scissors",
+  };
+
+  const randomKey = Math.floor(Math.random() * 3);
+  const choice = choices[String(randomKey)];
+
+  return choice;
+}
