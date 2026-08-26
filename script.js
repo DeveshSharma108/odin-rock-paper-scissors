870b571a555961af671c1e82cc05fa1b1b314550
DeveshSharma108 <137079248+DeveshSharma108@users.noreply.github.com>
2026-08-26 23:31:59 +0530

    add function to play a single round

--- script.js
+++ script.js
@@ -6,12 +6,73 @@ function getComputerChoice() {
   };
 
   const randomKey = Math.floor(Math.random() * 3);
-  const compChoice = choices[String(randomKey)];
+  const computerChoice = choices[String(randomKey)];
 
-  return compChoice;
+  return computerChoice;
 }
 
 function getHumanChoice() {
   const humanChoice = prompt("Enter your choice (case insensitive)");
   return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  let winner = null;
+  let msg;
+
+  switch (true) {
+    case humanChoice === "rock" && computerChoice === "scissors":
+      winner = "H";
+      break;
+
+    case humanChoice === "paper" && computerChoice === "rock":
+      winner = "H";
+      break;
+
+    case humanChoice === "scissors" && computerChoice === "paper":
+      winner = "H";
+      break;
+
+    case humanChoice === "scissors" && computerChoice === "rock":
+      winner = "C";
+      break;
+
+    case humanChoice === "rock" && computerChoice === "paper":
+      winner = "C";
+      break;
+
+    case humanChoice === "paper" && computerChoice === "scissors":
+      winner = "C";
+      break;
+
+    default:
+      break;
+  }
+
+  if (!winner) {
+    msg = `Human(${humanChoice})  vs Computer(${computerChoice})
+    It's a tie.
+    Human: ${humanScore} | Computer: ${computerScore}`;
+  } else if (winner === "H") {
+    humanScore += 1;
+    msg = `Human(${humanChoice})  vs Computer(${computerChoice})
+    ${humanChoice} beats ${computerChoice}.
+    Human: ${humanScore} | Computer: ${computerScore}`;
+  } else {
+    computerScore += 1;
+    msg = `Human(${humanChoice})  vs Computer(${computerChoice})
+    ${computerChoice} beats ${humanChoice}.
+    Human: ${humanScore} | Computer: ${computerScore}`;
+  }
+
+  console.log(msg);
+}
+
+let computerScore = 0;
+let humanScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
