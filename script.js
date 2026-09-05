6002be6e642ef7bdfd01d0fad40fa72c16eb1091
DeveshSharma108 <137079248+DeveshSharma108@users.noreply.github.com>
2026-09-06 01:54:51 +0530

    link game logic and UI, all state and interaction complete expcept power

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+let powerON = true;
+const emoji = { rock: "🪨", paper: "📄", scissors: "✂️", default: "❔" };
+
+let computerScore = 0;
+let playerScore = 0;
+
+const controlButtons = document.querySelectorAll(".control-buttons .button");
+const resetButton = document.querySelector(".system-buttons #reset");
+const powerButton = document.querySelector(".system-buttons #power");
+
+const playerScoreSpan = document.querySelector(".player-score span");
+const computerScoreSpan = document.querySelector(".computer-score span");
+
+const playerChoiceDisplay = document.querySelector(".player-choice-display p");
+const computerChoiceDisplay = document.querySelector(
+  ".computer-choice-display p"
+);
+
+const informationAreaDisplay = document.querySelector(".information-area p");
+
+controlButtons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const choice = btn.dataset.choice;
+    playGame(getplayerChoice(choice), getComputerChoice());
+  });
+});
+
+resetButton.addEventListener("click", reset);
+powerButton.addEventListener("click", togglePower);
+
 function getComputerChoice() {
   const choices = {
     0: "rock",
@@ -11,98 +41,76 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-function getHumanChoice() {
-  const humanChoice = prompt("Enter your choice (case insensitive)");
-  return humanChoice;
+function getplayerChoice(playerChoice) {
+  return playerChoice;
 }
 
-function playGame() {
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+
+  playerChoiceDisplay.textContent = "❔";
+  computerChoiceDisplay.textContent = "❔";
+
+  informationAreaDisplay.textContent = "Choose your weapon...";
+  playerScoreSpan.textContent = `${playerScore}/5`;
+  computerScoreSpan.textContent = `${computerScore}/5`;
+}
+
+function togglePower() {
+  powerON = !powerON;
+}
+
+function playGame(playerChoice, computerChoice) {
+  if (!powerON) {
+    return;
+  }
+  if (playerScore >= 5 || computerScore >= 5) {
+    return;
+  }
+
+  playRound(playerChoice, computerChoice);
+
+  function playRound(playerChoice, computerChoice) {
     let winner = null;
     let msg;
 
+    playerChoiceDisplay.textContent = emoji[playerChoice];
+    computerChoiceDisplay.textContent = emoji[computerChoice];
+
     switch (true) {
-      case humanChoice === "rock" && computerChoice === "scissors":
+      case playerChoice === computerChoice:
+        winner = null;
+        break;
+      case playerChoice === "rock" && computerChoice === "scissors":
         winner = "H";
         break;
 
-      case humanChoice === "paper" && computerChoice === "rock":
+      case playerChoice === "paper" && computerChoice === "rock":
         winner = "H";
         break;
 
-      case humanChoice === "scissors" && computerChoice === "paper":
+      case playerChoice === "scissors" && computerChoice === "paper":
         winner = "H";
-        break;
-
-      case humanChoice === "scissors" && computerChoice === "rock":
-        winner = "C";
-        break;
-
-      case humanChoice === "rock" && computerChoice === "paper":
-        winner = "C";
-        break;
-
-      case humanChoice === "paper" && computerChoice === "scissors":
-        winner = "C";
         break;
 
       default:
+        winner = "C";
         break;
     }
 
     if (!winner) {
-      msg = `Human(${humanChoice})  vs Computer(${computerChoice})
-    It's a tie.
-    Human: ${humanScore} | Computer: ${computerScore}`;
+      msg = `It's a tie.`;
     } else if (winner === "H") {
-      humanScore += 1;
-      msg = `Human(${humanChoice})  vs Computer(${computerChoice})
-    ${humanChoice} beats ${computerChoice}.
-    Human: ${humanScore} | Computer: ${computerScore}`;
+      playerScore += 1;
+      msg = `Player wins!`;
     } else {
       computerScore += 1;
-      msg = `Human(${humanChoice})  vs Computer(${computerChoice})
-    ${computerChoice} beats ${humanChoice}.
-    Human: ${humanScore} | Computer: ${computerScore}`;
+      msg = `CPU wins!`;
     }
 
-    console.log(msg);
-  }
-
-  let computerScore = 0;
-  let humanScore = 0;
-
-  let humanSelection = getHumanChoice();
-  let computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  humanSelection = getHumanChoice();
-  computerSelection = getComputerChoice();
-
-  playRound(humanSelection, computerSelection);
-
-  if (humanScore > computerScore) {
-    console.log("...... Human wins! ........");
-  } else {
-    console.log("...... Computer wins! ........");
+    informationAreaDisplay.textContent = msg;
+    playerScoreSpan.textContent = `${playerScore}/5`;
+    computerScoreSpan.textContent = `${computerScore}/5`;
   }
 }
-
-// playGame();
